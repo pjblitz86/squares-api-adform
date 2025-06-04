b63f76315cac0b0ad7c06441340ecbeb6eb8094b
pjblitz86 <paulius.juknis@gmail.com>
2025-06-04 10:26:25 +0300

    add Point model class

---
+++ squares-api-adform/Models/Point.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace squares_api_adform.Models
+
+{
+    public class Point
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int X { get; set; }
+
+        [Required]
+        public int Y { get; set; }
+    }
+}
